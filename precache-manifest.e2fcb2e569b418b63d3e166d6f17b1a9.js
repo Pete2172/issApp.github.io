19f231756219f1965fcef54a92346e6c46925e75
Pete2172 <p.kowalczyk.95@wp.pl>
2019-12-11 20:46:32 +0100

    Updates

---
+++ precache-manifest.e2fcb2e569b418b63d3e166d6f17b1a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93382d7228be73246adcecee41a62c08",
+    "url": "/issAppReact/index.html"
+  },
+  {
+    "revision": "7ef68f1d1af1cf7ec788",
+    "url": "/issAppReact/static/css/2.711045af.chunk.css"
+  },
+  {
+    "revision": "07e2ad148b1f19a7a907",
+    "url": "/issAppReact/static/css/main.6cfaa325.chunk.css"
+  },
+  {
+    "revision": "7ef68f1d1af1cf7ec788",
+    "url": "/issAppReact/static/js/2.36c885b0.chunk.js"
+  },
+  {
+    "revision": "c39a0e4a77bbec095d8cd65677ba0a6b",
+    "url": "/issAppReact/static/js/2.36c885b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07e2ad148b1f19a7a907",
+    "url": "/issAppReact/static/js/main.28e1bb2d.chunk.js"
+  },
+  {
+    "revision": "bb52d93b3a116b72df0a",
+    "url": "/issAppReact/static/js/runtime-main.ee6e2d62.js"
+  },
+  {
+    "revision": "6051ab523ab7d0312ab56cca53abf7a7",
+    "url": "/issAppReact/static/media/background.6051ab52.jpg"
+  }
+]);
